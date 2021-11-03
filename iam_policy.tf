d2f1779606b93f04c8379c7ce913a76e1e2036bd
Ethan Zhang <ethanzhang1997@gmail.com>
2021-11-03 04:24:53 -0400

    1

--- iam_policy.tf
+++ iam_policy.tf
@@ -10,47 +10,78 @@ resource "aws_iam_user_policy_attachment" "ghactions_iam_user_policy_attachment"
 
 data "aws_iam_policy_document" "ghactions_aws_iam_policy_document" {
   statement {
+    sid = ""
     actions = [
-        "ec2:AttachVolume",
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:CopyImage",
-        "ec2:CreateImage",
-        "ec2:CreateKeypair",
-        "ec2:CreateSecurityGroup",
-        "ec2:CreateSnapshot",
-        "ec2:CreateTags",
-        "ec2:CreateVolume",
-        "ec2:DeleteKeyPair",
-        "ec2:DeleteSecurityGroup",
-        "ec2:DeleteSnapshot",
-        "ec2:DeleteVolume",
-        "ec2:DeregisterImage",
-        "ec2:DescribeImageAttribute",
-        "ec2:DescribeImages",
-        "ec2:DescribeInstances",
-        "ec2:DescribeInstanceStatus",
-        "ec2:DescribeRegions",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeSnapshots",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeTags",
-        "ec2:DescribeVolumes",
-        "ec2:DetachVolume",
-        "ec2:GetPasswordData",
-        "ec2:ModifyImageAttribute",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:ModifySnapshotAttribute",
-        "ec2:RegisterImage",
-        "ec2:RunInstances",
-        "ec2:StopInstances",
-        "ec2:TerminateInstances"
-      ]
-    resources = ["*"]
+      "ec2:AttachVolume",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:DeregisterImage",
+      "ec2:DeleteTags",
+      "ec2:CreateImage",
+      "ec2:CreateVpc",
+      "ec2:CopyImage",
+      "ec2:ModifyImageAttribute",
+      "ec2:ReplaceIamInstanceProfileAssociation",
+      "ec2:UpdateSecurityGroupRuleDescriptionsIngress",
+      "ec2:DeleteVolume",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateSnapshot",
+      "ec2:ModifyInstanceAttribute",
+      "ec2:CreateInstanceExportTask",
+      "ec2:DetachVolume",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:UpdateSecurityGroupRuleDescriptionsEgress",
+      "ec2:TerminateInstances",
+      "ec2:CreateTags",
+      "ec2:RegisterImage",
+      "ec2:RunInstances",
+      "ec2:ModifySecurityGroupRules",
+      "ec2:StopInstances",
+      "ec2:CreateVolume",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:GetPasswordData",
+      "ec2:DescribeImageAttribute",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DeleteVpc",
+      "ec2:AssociateIamInstanceProfile",
+      "ec2:ImportKeyPair",
+      "ec2:CreateKeyPair",
+      "ec2:DeleteKeyPair"
+    ]
+    resources = ["arn:aws:ec2:us-east-1:${var.user_id}:network-interface/*",
+      "arn:aws:ec2:us-east-1::image/*",
+      "arn:aws:ec2:us-east-1:${var.user_id}:instance/*",
+      "arn:aws:ec2:us-east-1:${var.user_id}:vpc/*",
+      "arn:aws:ec2:us-east-1:${var.user_id}:security-group/*",
+      "arn:aws:ec2:us-east-1:${var.user_id}:volume/*",
+      "arn:aws:ec2:us-east-1:${var.user_id}:subnet/*",
+    "arn:aws:ec2:us-east-1:${var.user_id}:key-pair/*"]
     effect = "Allow"
   }
   statement {
-    actions   = ["iam:PassRole", "iam:GetInstanceProfile", "iam:CreateServiceLinkedRole"]
+    actions = ["ec2:DescribeInstances",
+      "ec2:DescribeIamInstanceProfileAssociations",
+      "ec2:DescribeTags",
+      "ec2:DescribeRegions",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeImages",
+      "ec2:CreateDefaultVpc",
+      "ec2:DescribeSecurityGroupRules",
+      "ec2:DescribeSecurityGroupReferences",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeKeyPairs",
+      "ec2:DescribeInstanceStatus",
+    "ec2:DescribeStaleSecurityGroups"]
     resources = ["*"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions   = ["iam:PassRole"]
+    resources = ["arn:aws:iam::${var.user_id}:role/*"]
     effect    = "Allow"
   }
 }
