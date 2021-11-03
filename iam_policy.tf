44451c706c03732e453ef1161bb6ca9a280a9150
Ethan Zhang <ethanzhang1997@gmail.com>
2021-11-03 04:09:33 -0400

    change policy

--- iam_policy.tf
+++ iam_policy.tf
@@ -10,29 +10,47 @@ resource "aws_iam_user_policy_attachment" "ghactions_iam_user_policy_attachment"
 
 data "aws_iam_policy_document" "ghactions_aws_iam_policy_document" {
   statement {
-    sid = ""
     actions = [
-      "ec2:DeregisterImage",
-      "ec2:CreateImage",
-      "ec2:CopyImage",
-      "ec2:ModifyImageAttribute",
-      "ec2:ReplaceIamInstanceProfileAssociation",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:CreateInstanceExportTask",
-      "ec2:TerminateInstances",
-      "ec2:RegisterImage",
-      "ec2:RunInstances",
-      "ec2:StopInstances",
-      "ec2:DescribeImageAttribute",
-      "ec2:AssociateIamInstanceProfile",
-    ]
-    resources = ["arn:aws:ec2:us-east-1::image/*",
-      "arn:aws:ec2:us-east-1:${var.user_id}:instance/*"]
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CopyImage",
+        "ec2:CreateImage",
+        "ec2:CreateKeypair",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteKeyPair",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteVolume",
+        "ec2:DeregisterImage",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeRegions",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume",
+        "ec2:GetPasswordData",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:RegisterImage",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances"
+      ]
+    resources = ["*"]
     effect = "Allow"
   }
   statement {
-    actions   = ["iam:PassRole"]
-    resources = ["arn:aws:iam::${var.user_id}:role/*"]
+    actions   = ["iam:PassRole", "iam:GetInstanceProfile", "iam:CreateServiceLinkedRole"]
+    resources = ["*"]
     effect    = "Allow"
   }
 }
